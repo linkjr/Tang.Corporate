2a08a32df7146b466bf3f9cbefa7ac7f54bbdd5a
linkjr <linkjr@sina.com>
2015-02-12 23:49:46 +0800

    Add file.

---
+++ Tang.Corporate.Web/Areas/Admin/Controllers/NewsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using Tang.Corporate.DataObjects;
+
+namespace Tang.Corporate.Web.Areas.Admin.Controllers
+{
+    public class NewsController : Controller
+    {
+        // GET: Admin/News
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(ManageNewsDataObject dataObject)
+        {
+            return View();
+        }
+    }
+}
