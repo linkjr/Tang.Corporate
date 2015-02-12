9df86da784bafbc25c6d14e8e1b3d140a12bad38
linkjr <linkjr@sina.com>
2015-02-12 23:48:31 +0800

    Update.

--- Tang.Corporate.DataObjects/NewsDataObject.cs
+++ Tang.Corporate.DataObjects/NewsDataObject.cs
@@ -15,43 +15,87 @@ namespace Tang.Corporate.DataObjects
         /// 获取或设置新闻ID。
         /// </summary>
         [Display(Name = "新闻ID")]
+        [DataMember]
         public Guid ID { get; set; }
 
         /// <summary>
         /// 获取或设置新闻标题。
         /// </summary>
         [Display(Name = "标题")]
+        [DataMember]
         public string Title { get; set; }
 
         /// <summary>
         /// 获取或设置封面图。
         /// </summary>
         [Display(Name = "封面图")]
+        [DataMember]
         public string CoverImg { get; set; }
 
         /// <summary>
         /// 获取或设置新闻内容。
         /// </summary>
         [Display(Name = "内容")]
+        [DataMember]
         public string Content { get; set; }
 
         /// <summary>
         /// 获取或设置创建人ID。
         /// </summary>
         [Display(Name = "创建人")]
+        [DataMember]
         public Guid UserID { get; set; }
 
         /// <summary>
         /// 获取或设置创建日期。
         /// </summary>
         [Display(Name = "创建日期")]
+        [DataMember]
         public DateTime CreateDate { get; set; }
     }
 
     [DataContract]
     public class ManageNewsDataObject
     {
+        /// <summary>
+        /// 获取或设置新闻ID。
+        /// </summary>
+        [DataMember]
+        public Guid ID { get; set; }
 
+        /// <summary>
+        /// 获取或设置新闻标题。
+        /// </summary>
+        [Display(Name = "标题")]
+        [Required(ErrorMessage = "{0} 不能为空。")]
+        [StringLength(50, ErrorMessage = "{0} 长度不能超过{1}。")]
+        [DataMember]
+        public string Title { get; set; }
+
+        /// <summary>
+        /// 获取或设置封面图。
+        /// </summary>
+        [Display(Name = "封面图")]
+        [Required(ErrorMessage = "{0} 不能为空。")]
+        [StringLength(200, ErrorMessage = "{0} 长度不能超过{1}。")]
+        [DataMember]
+        public string ConverImg { get; set; }
+
+        /// <summary>
+        /// 获取或设置视频地址。
+        /// </summary>
+        [Display(Name = "视频地址")]
+        [StringLength(200, ErrorMessage = "{0} 长度不能超过{1}。")]
+        [DataMember]
+        public string VideoUrl { get; set; }
+
+        /// <summary>
+        /// 获取或设置新闻内容。
+        /// </summary>
+        [Display(Name = "内容")]
+        [Required(ErrorMessage = "{0} 不能为空。")]
+        [DataMember]
+        public string Content { get; set; }
     }
 
     [DataContract]
